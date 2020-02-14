da3ffd1e22d0c97f2aefa8651c0bd053fec40158
wpcreativity <58761152+wpcreativity@users.noreply.github.com>
2020-02-14 18:16:09 +0530

    gjhg

---
+++ about-us.php
@@ -0,0 +1,45 @@
+<?php include 'include/header.php'; ?>
+<div class="media-page my-5 px-md-5">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 mb-4">
+                <div class="media-box d-flex flex-row">
+                    <figure>
+                        <a href="https://www.hotboxinpodcast.com/"><img src="images/BlahSquareCollage.png" alt=""></a>
+                    </figure>
+                    <div class="media-text">
+                        <h1>Hotboxin’
+                            Podcast</h1>
+                        <p>Mike has always made choices in his personal style that are different. He was the first one to really start wearing Versace, to drive Rolls Royce’s and to use a tiger as an accessory. He sets trends almost effortlessly.</p>
+                        <a href="https://www.hotboxinpodcast.com/">Visit site <i class="ti-arrow-right ml-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 mb-4">
+                <div class="media-box d-flex flex-row">
+                    <figure>
+                        <a href="https://tysonranch.com/"> <img src="images/MIKE_-2.png" alt=""></a>
+                    </figure>
+                    <div class="media-text">
+                        <h1>tyson ranch</h1>
+                        <p>Mike has always made choices in his personal style that are different. He was the first one to really start wearing Versace, to drive Rolls Royce’s and to use a tiger as an accessory. He sets trends almost effortlessly.</p>
+                        <a href="https://tysonranch.com/">Visit site <i class="ti-arrow-right ml-2"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 mb-4">
+                <div class="media-box d-flex flex-row">
+                    <figure>
+                        <a href="https://dwiink.com/"> <img src="images/Dwink-Still-02.png" alt=""></a>
+                    </figure>
+                    <div class="media-text">
+                        <h1>DWiiNK</h1>
+                        <p>Mike has always made choices in his personal style that are different. He was the first one to really start wearing Versace, to drive Rolls Royce’s and to use a tiger as an accessory. He sets trends almost effortlessly.</p>
+                        <a href="https://dwiink.com/">Visit site <i class="ti-arrow-right ml-2"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'include/footer.php'; ?>
